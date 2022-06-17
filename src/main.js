776de80bb192bb630aea891e03a5cae3deaa38e8
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-06-17 21:34:28 +0900

    fix: eslintのルール追加による整形

--- src/main.js
+++ src/main.js
@@ -13,21 +13,21 @@ var betterJapanese = {
     fallbackTimer: 0,
     origins: {},
 
-    init: function () {
+    init: function() {
         this.load()
 
-        //万進法用の単位
-        betterJapanese.formatJpPrefixes = ['','万','億','兆','京','垓','秭','穣','溝','澗','正','載','極','恒河沙']
-        //上数用の単位
-        betterJapanese.formatJpSuffixes = [];
-        for(let suf of ['頻波羅','矜羯羅','阿伽羅']){
+        // 万進法用の単位
+        betterJapanese.formatJpPrefixes = ['', '万', '億', '兆', '京', '垓', '秭', '穣', '溝', '澗', '正', '載', '極', '恒河沙']
+        // 上数用の単位
+        betterJapanese.formatJpSuffixes = []
+        for(let suf of ['頻波羅', '矜羯羅', '阿伽羅']) {
             let len = betterJapanese.formatJpSuffixes.push(suf) - 1
-            for(let i = 0; i < len; i++){
+            for(let i = 0; i < len; i++) {
                 betterJapanese.formatJpSuffixes.push(betterJapanese.formatJpSuffixes[i] + suf)
             }
         }
         betterJapanese.formatJpSuffixes = ['', ...betterJapanese.formatJpSuffixes]
-        //塵劫記用の単位
+        // 塵劫記用の単位
         betterJapanese.formatJpShort = [...betterJapanese.formatJpPrefixes, '阿僧祇', '那由多', '不可思議', '無量大数']
 
         this.fallbackTimer = setTimeout(() => {
@@ -40,45 +40,45 @@ var betterJapanese = {
         this.log('Initialized')
     },
 
-    initAfterLoad: function(){
+    initAfterLoad: function() {
         betterJapanese.origins.updateMenu = Game.UpdateMenu
         betterJapanese.origins.sayTime = Game.sayTime
         betterJapanese.origins.beautify = Beautify
 
         // メニューに独自ボタンを実装
-        Game.UpdateMenu = function(){
+        Game.UpdateMenu = function() {
             betterJapanese.origins.updateMenu()
-            if(Game.onMenu == 'prefs'){
+            if(Game.onMenu == 'prefs') {
                 betterJapanese.injectMenu()
             }
         }
 
         // 時間表記からカンマを取り除く
-        Game.sayTime = function(time, detail){
+        Game.sayTime = function(time, detail) {
             return betterJapanese.origins.sayTime(time, detail).replaceAll(', ', '')
         }
         
-        //接尾辞挿入の4桁区切り版、secondで第二単位の使用を指定
+        // 接尾辞挿入の4桁区切り版、secondで第二単位の使用を指定
         betterJapanese.formatEveryFourthPower = function(prefixes, suffixes, second)
         {
-            return function (value)
+            return function(value)
             {
-                if(!isFinite(value)) return '無限大';//loc("Infinity")
-                if(value > 10 ** (prefixes.length * suffixes.length * 4)){
+                if(!isFinite(value)) return '無限大'// loc("Infinity")
+                if(value > 10 ** (prefixes.length * suffixes.length * 4)) {
                     return value.toPrecision(3).toString()
                 }
-                if(value < 1){//小数点の場合は最大小数第3位まででそのまま出力
+                if(value < 1) {// 小数点の場合は最大小数第3位まででそのまま出力
                     return (Math.round(value * 1000) / 1000).toString()
                 }
                 let numeral = Math.floor(Math.log10(value) / 4)
                 let preIndex = numeral % prefixes.length
                 let sufIndex = Math.floor(numeral / prefixes.length)
                 let dispNum = Math.round(value * 10000 / (10 ** (numeral * 4)))
-                if(second){
-                    if(preIndex){
+                if(second) {
+                    if(preIndex) {
                         return Math.floor(dispNum / 10000) + prefixes[preIndex] + (dispNum % 10000) + prefixes[preIndex - 1] + suffixes[sufIndex]
                     }else{
-                        if(sufIndex){
+                        if(sufIndex) {
                             return Math.floor(dispNum / 10000) + suffixes[sufIndex] + (dispNum % 10000) + prefixes[prefixes.length - 1] + suffixes[sufIndex - 1]
                         }else{
                             return value
@@ -87,12 +87,11 @@ var betterJapanese = {
                 }else{
                     return Math.round(value * 10000 / (10 ** (numeral * 4))) / 10000 + prefixes[preIndex] + suffixes[sufIndex]
                 }
-            };
+            }
         }
         
-        //本家の挿入関数に追加
-        numberFormatters=
-        [
+        // 本家の挿入関数に追加
+        numberFormatters = [
             formatEveryThirdPower(formatShort),
             formatEveryThirdPower(formatLong),
             rawFormatter,
@@ -100,82 +99,82 @@ var betterJapanese = {
             betterJapanese.formatEveryFourthPower(betterJapanese.formatJpShort, [''], true),
             betterJapanese.formatEveryFourthPower(betterJapanese.formatJpPrefixes, betterJapanese.formatJpSuffixes, false),
             betterJapanese.formatEveryFourthPower(betterJapanese.formatJpPrefixes, betterJapanese.formatJpSuffixes, true)
-        ];
+        ]
 
-        //設定によって日本語単位を使用するように変更、同時にカンマ区切りも場合によって変更
-        Beautify = function(val, floats){
-            var negative = (val < 0);
-            var decimal = '';
-            var fixed = val.toFixed(floats);
-            if (floats > 0 && Math.abs(val) < 1000 && Math.floor(fixed) != fixed) decimal = '.' + (fixed.toString()).split('.')[1];
-            val = Math.floor(Math.abs(val));
-            if (floats > 0 && fixed == val + 1) val++;
-            let format = 0;
-            if(Game.prefs.format){
-                format = 2;
+        // 設定によって日本語単位を使用するように変更、同時にカンマ区切りも場合によって変更
+        Beautify = function(val, floats) {
+            var negative = (val < 0)
+            var decimal = ''
+            var fixed = val.toFixed(floats)
+            if (floats > 0 && Math.abs(val) < 1000 && Math.floor(fixed) != fixed) decimal = '.' + (fixed.toString()).split('.')[1]
+            val = Math.floor(Math.abs(val))
+            if (floats > 0 && fixed == val + 1) val++
+            let format = 0
+            if(Game.prefs.format) {
+                format = 2
             }else{
-                if(betterJapanese.config.numberJP){
-                    if(betterJapanese.config.shortFormatJP){
-                        format = 3;
+                if(betterJapanese.config.numberJP) {
+                    if(betterJapanese.config.shortFormatJP) {
+                        format = 3
                     }else{
-                        format = 5;
+                        format = 5
                     }
-                    if(betterJapanese.config.secondFormatJP){
-                        format++;
+                    if(betterJapanese.config.secondFormatJP) {
+                        format++
                     }
                 }else{
-                    format = 1;
+                    format = 1
                 }
             }
-            var formatter=numberFormatters[format];
-            var output=(val.toString().indexOf('e+')!=-1 && format==2)?val.toPrecision(3).toString():formatter(val).toString();
-            if(Game.prefs.format || (betterJapanese.config.numberJP && betterJapanese.config.secondFormatJP)){
-                output = output.replace(/\B(?=(\d{3})+(?!\d))/g,',');
+            var formatter = numberFormatters[format]
+            var output = (val.toString().indexOf('e+') != -1 && format == 2) ? val.toPrecision(3).toString() : formatter(val).toString()
+            if(Game.prefs.format || (betterJapanese.config.numberJP && betterJapanese.config.secondFormatJP)) {
+                output = output.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
             }else{
-                output = output.replace(/^(\d)(\d{3})/,'$1,$2');
+                output = output.replace(/^(\d)(\d{3})/, '$1,$2')
             }
-            if (output=='0') negative=false;
-            return negative?'-'+output:output+decimal;
+            if (output == '0') negative = false
+            return negative ? '-' + output : output + decimal
         }
 
-        //背景の名前を翻訳
-        for(let i=1;i<Game.AllBGs.length;i++){
-            Game.AllBGs[i].enName = Game.AllBGs[i].name;
-            Game.AllBGs[i].name = loc(Game.AllBGs[i].enName);
+        // 背景の名前を翻訳
+        for(let i = 1; i < Game.AllBGs.length; i++) {
+            Game.AllBGs[i].enName = Game.AllBGs[i].name
+            Game.AllBGs[i].name = loc(Game.AllBGs[i].enName)
         }
 
         // hookを削除
         Game.removeHook('create', betterJapanese.initAfterLoad)
     },
 
-    register: function () {
+    register: function() {
         Game.registerMod(this.name, this)
         Game.registerHook('create', betterJapanese.initAfterLoad)
     },
 
-    save: function () {
+    save: function() {
         localStorage.setItem('BJPConfig', JSON.stringify(this.config))
     },
 
-    load: function () {
+    load: function() {
         var conf = localStorage.getItem('BJPConfig')
         if (conf) this.config = JSON.parse(conf)
     },
 
-    injectMenu: function () {
+    injectMenu: function() {
         let updateAll = () => {
             BeautifyAll()
             Game.RefreshStore()
-            Game.upgradesToRebuild=1
+            Game.upgradesToRebuild = 1
         }
         this.writeButton('toggleBJPButton', 'enable', '日本語訳の改善', '日本語訳を非公式翻訳版に置き換えます', updateAll)
         this.writeButton('toggleNumberJPButton', 'numberJP', '日本語単位', '数の単位に日本語単位を用います', updateAll)
         this.writeButton('toggleShortFormatJPButton', 'shortFormatJP', '塵劫記単位', '数の単位に塵劫記の単位(阿僧祇～無量大数)を用います', updateAll)
-        this.writeButton('toggleSecondFormatJPButton', 'secondFormatJP', '第二単位', `${loc("ON")}の場合はXXXX億YYYY万、${loc("OFF")}の場合はXXXX.YYYY億のように表示されます`, updateAll)
+        this.writeButton('toggleSecondFormatJPButton', 'secondFormatJP', '第二単位', `${loc('ON')}の場合はXXXX億YYYY万、${loc('OFF')}の場合はXXXX.YYYY億のように表示されます`, updateAll)
     },
 
-    writeButton: function (buttonId, targetProp, desc, label = null, callback = null, targetElementName = 'monospaceButton') {
-        //本家のWritePrefButtonとほぼ同じ
+    writeButton: function(buttonId, targetProp, desc, label = null, callback = null, targetElementName = 'monospaceButton') {
+        // 本家のWritePrefButtonとほぼ同じ
 
         // ボタンを追加する先の要素を指定 (デフォルトはmonospaceButton)
         var targetElement = l(targetElementName)
@@ -195,7 +194,7 @@ var betterJapanese = {
 
         elementButton.setAttribute(Game.clickStr, onclickStr)
         
-        elementButton.innerText = `${desc} ${this.config[targetProp] ? loc("ON") : loc("OFF")}`
+        elementButton.innerText = `${desc} ${this.config[targetProp] ? loc('ON') : loc('OFF')}`
         targetElement.parentNode.insertBefore(elementButton, targetElement.previousElementSibling)
 
         // ラベルがあれば生成
@@ -206,21 +205,21 @@ var betterJapanese = {
         }
     },
 
-    toggleButton: function (buttonId, targetProp, desc) {
+    toggleButton: function(buttonId, targetProp, desc) {
         var button = l(buttonId)
         betterJapanese.config[targetProp] = !betterJapanese.config[targetProp]
         button.className = `smallFancyButton prefButton option ${this.config[targetProp] ? 'on' : 'off'}`
-        button.innerText = `${desc} ${this.config[targetProp] ? loc("ON") : loc("OFF")}`
+        button.innerText = `${desc} ${this.config[targetProp] ? loc('ON') : loc('OFF')}`
         PlaySound('snd/tick.mp3')
     },
 
-    addDevButton: function () {
+    addDevButton: function() {
         var element = document.createElement('div')
-        element.innerHTML = `<button style="position: absolute; left: 10px; top: 10px; z-index: 9999;" type="button" onclick="betterJapanese.reloadLanguagePack()">Reload LanguageFile</button>`
+        element.innerHTML = '<button style="position: absolute; left: 10px; top: 10px; z-index: 9999;" type="button" onclick="betterJapanese.reloadLanguagePack()">Reload LanguageFile</button>'
         document.body.append(element)
     },
 
-    checkUpdate: async function () {
+    checkUpdate: async function() {
         this.log('Checking updates')
 
         if(this.isDev) return await this.updateLanguagePack(this.apiUrl.dev)
@@ -240,26 +239,26 @@ var betterJapanese = {
         }
     },
 
-    showUpdateNotification: function () {
+    showUpdateNotification: function() {
         Game.Notify('日本語訳改善Mod', '翻訳データを更新しました。<br>再読み込み後から有効になります。<br><a onclick="betterJapanese.reload()">セーブデータを保存して再読み込み</a>')
     },
 
-    reload: function () {
+    reload: function() {
         Game.toSave = true
         Game.toReload = true
     },
 
-    reloadLanguagePack: async function () {
+    reloadLanguagePack: async function() {
         await this.checkUpdate()
         this.showUpdateNotification()
         ModLanguage('JA', JSON.parse(localStorage.getItem('BJPLangPack')))
     },
 
-    updateLanguagePack: async function (url) {
+    updateLanguagePack: async function(url) {
         var base = {
-            "": {
-                "language": "JA",
-                "plural-forms": "nplurals=2;plural=(n!=1);"
+            '': {
+                'language': 'JA',
+                'plural-forms': 'nplurals=2;plural=(n!=1);'
             },
         }
 
@@ -275,7 +274,7 @@ var betterJapanese = {
         return true
     },
 
-    log: function (msg) {
+    log: function(msg) {
         console.log(`%c[BetterJapanese]%c ${msg}`, 'color: yellow', '')
     }
 }
