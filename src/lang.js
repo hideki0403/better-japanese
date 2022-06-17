db27a476221cf04b3421b602bffe780cfcd8ce21
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-06-18 02:01:27 +0900

    change: 変数の変更に伴う修正

--- src/lang.js
+++ src/lang.js
@@ -1,10 +1,10 @@
 console.time('BetterJapanese')
 console.log('[BetterJapanese/Language] Loading')
 
-var conf = JSON.parse(localStorage.getItem('BJPConfig') || '{enable: false}')
+var conf = JSON.parse(localStorage.getItem('BJPConfig') || '{replaceJP: false}')
 var language = localStorage.getItem('BJPLangPack')
 
-if (language && conf.enable) ModLanguage('JA', JSON.parse(language))
+if (language && conf.replaceJP) ModLanguage('JA', JSON.parse(language))
 
 console.log('[BetterJapanese/Language] Loaded')
 console.timeEnd('BetterJapanese')
