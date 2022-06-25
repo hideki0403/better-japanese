aeb9ffdf116c43945817a5e6f326fe52d8ea040d
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-06-25 14:41:40 +0900

    fix: 翻訳が反映されない問題を修正

--- src/web/JA.js
+++ src/web/JA.js
@@ -1,10 +1,15 @@
 // BetterJapanese Injector
 
-LoadLang(`loc/JA_fallback.js?v=${Game.version}`)
-
 let conf = JSON.parse(localStorage.getItem('BJPConfig') || '{"replaceJP": false}')
 let language = localStorage.getItem('BJPLangPack')
 
-if (language && conf.replaceJP) ModLanguage('JA', JSON.parse(language))
+// localStorageに翻訳データが保存されていない場合はfallbackを使用
+if (!language) {
+    LoadLang(`loc/JA_fallback.js?v=${Game.version}`)
+}
+
+if (language && conf.replaceJP) {
+    AddLanguage('JA', 'japanese', JSON.parse(language))
+}
 
 Game.LoadMod(`https://pages.yukineko.me/better-japanese/assets/main.js?v=${Game.version}`)
