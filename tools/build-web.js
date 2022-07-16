539f7f7ea566e37e4032f69d3b4f02f1a33a04aa
yukineko <27853966+hideki0403@users.noreply.github.com>
2022-07-16 16:07:13 +0900

    add: ビルド時にversion.jsonを生成するように

--- tools/build-web.js
+++ tools/build-web.js
@@ -6,8 +6,11 @@ const assetsDir = path.join(distDir, './assets/')
 const srcDir = path.join(rootDir, './src/common/')
 const webAssetsDir = path.join(rootDir, './src/web/')
 
+const version = require(path.join(rootDir, './package.json')).version
+
 if (!fs.existsSync(distDir)) fs.mkdirSync(distDir)
 if (!fs.existsSync(assetsDir)) fs.mkdirSync(assetsDir)
 
 fs.copySync(srcDir, assetsDir)
 fs.copySync(webAssetsDir, distDir)
+fs.writeJSONSync(path.join(distDir, './version.json'), { version })
